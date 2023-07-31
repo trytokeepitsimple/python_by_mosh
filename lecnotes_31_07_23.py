5b719c2e23d1f3cf34a4ead8af6c9e526a3b3549
StarLord <learning.blaze224@gmail.com>
2023-07-31 15:25:08 +0530

    lec_31_07

---
+++ lecnotes_31_07_23.py
@@ -0,0 +1,18 @@
+# Define string which are of multiple lines
+
+# triple quotes - use triple quotes to write multiple line string
+
+# These quotes either could be a single or double quotes
+
+#Example 1 :
+
+# message = '''
+# Hi Customer ,
+# Glad to meet you on this
+# Thanks ,
+# Support Team
+# '''
+#
+# print(message)
+
+
